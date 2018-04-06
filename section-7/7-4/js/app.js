f4ff575ed12c5f27a3b467e9373b3e4b05b5c925
sxuc3s <cody_sechelski@fanniemae.com>
2018-04-06 13:11:50 -0500

    adding code for 7.5

--- section-7/7-4/js/app.js
+++ section-7/7-4/js/app.js
@@ -1,11 +1,67 @@
-function UI(){}
-UI.prototype.showAlert = function(msg,alertType){
-  let alert = document.createElement('div');
+document.getElementById('UI-btn-get-customer').addEventListener('click', loadCustomer);
+//document.getElementById('UI-btn-get-customers').addEventListener('click',loadCustomers);
+
+function loadCustomer(e) {
+  const ui = new UI;
+  const xhr = new XMLHttpRequest();
+
+  xhr.open('GET', 'data/customer.json', true);
+  xhr.onload = function () {
+    if (this.status === 200) {
+      ui.displayResults(JSON.parse(this.responseText))
+    } else {
+      ui.showAlert(`Woops! Error: ${this.status}-${this.statusText}`);
+    }
+  }
+  xhr.send();
+}
+
+
+function UI() {}
+UI.prototype.showAlert = function (msg, alertType) {
+  const alert = document.createElement('div');
   alert.className = `alert ${alertType}`;
   alert.textContent = msg;
   document.getElementById('alerts').appendChild(alert);
 
-  setTimeout(function(){
+  setTimeout(function () {
     alert.remove();
-  }, 3000);  
+  }, 3000);
+}
+UI.prototype.displayResults = function (customers) {
+  const
+    output = document.getElementById('output');
+    title = document.createElement('h3'),
+    collection = document.createElement('ul');
+
+    output.innerHTML = '';
+
+  title.textContent = 'Customers';
+
+  if (Array.isArray(customers)) {
+    customers.forEach(function (customer) {
+      const item = document.createElement('li');
+      item.innerHTML = `
+        ${customer.name}
+        <ul>
+          <li><strong>Company:</strong> ${customer.company}</li>
+          <li><strong>Phone:</strong> ${customer.phone}</li>
+        </ul>
+      `;
+      collection.appendChild(item);
+    });
+  } else {
+    const item = document.createElement('li');
+    item.innerHTML = `
+        ${customers.name}
+        <ul>
+          <li><strong>Company:</strong> ${customers.company}</li>
+          <li><strong>Phone:</strong> ${customers.phone}</li>
+        </ul>
+      `;
+    collection.appendChild(item);
+  }
+
+  output.appendChild(title);
+  output.appendChild(collection);
 }
--- section-7/7-5/js/app.js
+++ section-7/7-5/js/app.js
@@ -1,31 +1,4 @@
-document.getElementById('button').addEventListener('click',loadData);
 
-function loadData(){
-  //create an XHR Object
-  const xhr = new XMLHttpRequest();
-
-  xhr.open('GET','data/data.txt',true);
-
-  xhr.onprogress = function(){
-    document.getElementById('button').style.background = 'red';
-  }
-
-  xhr.onload = function(){
-    const ui = new UI;
-    console.log(this);
-    if(this.status === 200){
-      console.log(this.responseText);
-      document.getElementById('output').textContent = this.responseText;
-      ui.showAlert('Data retrieved', 'success');
-    }
-    else{
-      ui.showAlert(`${this.statusText}.Error code ${this.status}`, 'error');
-    }
-    document.getElementById('button').style.background = 'transparent';
-  }
-
-  xhr.send();
-}
 
 function UI(){}
 UI.prototype.showAlert = function(msg,alertType){
