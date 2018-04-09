643c82681d1e1c33dced4cba35116182ffaecb56
Cody <codysechelski@gmail.com>
2018-04-09 00:05:30 -0500

    updating 9.2

---
+++ section-9/9-1/js/app.js
@@ -0,0 +1,7 @@
+try {
+  myfunction();
+} catch (e) {
+  console.log(e);
+  console.log(e.message);
+  console.log(e.name);
+}
---
+++ section-9/9-2/js/app.js
@@ -0,0 +1,22 @@
+let re;
+re = /hello/i;
+
+//console.log(re);
+//console.log(re.source);
+
+// const result = re.exec('hello world hello');
+// console.log(result);
+
+//const result = re.test('Hello world');
+//console.log(result);
+
+//const str = 'Hello There';
+//const result = str.match(re);
+//console.log(result);
+
+
+//returns -1 if none found
+const str = 'Hello There';
+const result = str.search(re);
+console.log(result);
+
