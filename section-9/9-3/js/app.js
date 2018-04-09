2c3f4a8d955b4a9b50a096935f3723f61f73e525
sxuc3s <cody_sechelski@fanniemae.com>
2018-04-09 16:04:55 -0500

    adding 9.3 (complete) and 9.4

---
+++ section-9/9-3/js/app.js
@@ -0,0 +1,62 @@
+let re;
+let str;
+
+
+
+// re = /hello/;
+// str = 'Hello world';
+
+// re = /hello/;
+// str = 'hello world';
+
+// re = /hello/i;
+// str = 'Hello world';
+
+//Metacharacter Symbols
+////////////////////////////////////////////////////////
+// re = /^h/i;     //Must start with
+// str = 'Hello World';
+
+// re = /world$/i;     //Must end with
+// str = 'Hello World';
+
+// re = /^hello$/i;     //Must start and end with
+// str = 'Hello';
+
+// re = /^h.llo$/i;     //Must any one character
+// //str = 'Hello';
+// str = 'H3llo';
+
+// re = /h*llo/i;     //Must any character 0 or more times
+// //str = 'Hello';
+// str = 'H323llo';
+
+// re = /gre?a?y/;       //Option character
+// ['gray', 'grey', 'gr3y', "greay", 'gry'].forEach(function(item){
+//   str = item;
+//   reTest(re, str);
+// });
+
+ re = /gre?a?y\?/;       //Escape Characters
+['gray', 'grey', 'gray?', 'grey?'].forEach(function(item){
+  str = item;
+  reTest(re, str);
+});
+////////////////////////////////////////////////////////
+
+
+// const result = re.exec(str);
+// console.log(result);
+
+
+
+function reTest(re, str){
+  if(re.test(str)){
+    console.log(`${str} matches ${re.source}`);
+  }
+  else{
+    console.log(`${str} does NOT match ${re.source}`);
+  }
+}
+
+//reTest(re, str);
