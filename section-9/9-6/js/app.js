b68a99f69c01c1c61eff9235f37d4985878a85a7
Cody <codysechelski@gmail.com>
2018-04-11 00:02:58 -0500

    added the base project files for section 10. Added completed files for 10.1

--- section-9/9-6/js/app.js
+++ section-9/9-6/js/app.js
@@ -5,44 +5,29 @@ document.getElementById('phone').addEventListener('blur', validatePhone);
 
 function validateName(e) {
   const re = /^[a-zA-Z]{2,10}$/;
-
-  if (!re.test(e.target.value)) {
-    e.target.classList.add('is-invalid');
-  } else {
-    e.target.classList.remove('is-invalid');
-  }
+  validateField(e.target, re);
 }
 
 function validateZip(e) {
   const re = /^[0-9]{5}(-[0-9]{4})?$/;
-
-  if (!re.test(e.target.value)) {
-    e.target.classList.add('is-invalid');
-  } else {
-    e.target.classList.remove('is-invalid');
-  }
+  validateField(e.target, re);
 }
 
 function validateEmail(e) {
   const re = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
-
-  if (!re.test(e.target.value)) {
-    e.target.classList.add('is-invalid');
-  } else {
-    e.target.classList.remove('is-invalid');
-  }
+  validateField(e.target, re);
 }
 
 function validatePhone(e) {
   const re = /^\(?\d{3}\)?[-. ]?\d{3}[-. ]?\d{4}$/;
-
-  if (!re.test(e.target.value)) {
-    e.target.classList.add('is-invalid');
-  } else {
-    e.target.classList.remove('is-invalid');
-  }
+  validateField(e.target, re);
 }
 
-function validateField(target,expression){
-
+function validateField(target, expression) {
+  
+  if (!expression.test(target.value)) {
+    target.classList.add('is-invalid');
+  } else {
+    target.classList.remove('is-invalid');
+  }
 }
