ccb53ba2c2cf9926d927e338629f84952031f482
sxuc3s <cody_sechelski@fanniemae.com>
2018-04-12 08:51:02 -0500

    updating a file

--- section-9/9-6/js/app.js
+++ section-9/9-6/js/app.js
@@ -1,33 +1,27 @@
-document.getElementById('name').addEventListener('blur', validateName);
-document.getElementById('zip').addEventListener('blur', validateZip);
-document.getElementById('email').addEventListener('blur', validateEmail);
-document.getElementById('phone').addEventListener('blur', validatePhone);
+document.getElementById('name').addEventListener('blur', function(e){validateField(e.target)});
+document.getElementById('zip').addEventListener('blur', function(e){validateField(e.target)});
+document.getElementById('email').addEventListener('blur', function(e){validateField(e.target)});
+document.getElementById('phone').addEventListener('blur', function(e){validateField(e.target)});
 
-function validateName(e) {
-  const re = /^[a-zA-Z]{2,10}$/;
-  validateField(e.target, re);
-}
 
-function validateZip(e) {
-  const re = /^[0-9]{5}(-[0-9]{4})?$/;
-  validateField(e.target, re);
-}
-
-function validateEmail(e) {
-  const re = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
-  validateField(e.target, re);
-}
-
-function validatePhone(e) {
-  const re = /^\(?\d{3}\)?[-. ]?\d{3}[-. ]?\d{4}$/;
-  validateField(e.target, re);
-}
-
-function validateField(target, expression) {
+function validateField(target) {
+  console.log(target);
   
+  let expression = validationRules[target.id].exp;
+  let message = validationRules[target.id].msg;
   if (!expression.test(target.value)) {
+    target.nextElementSibling.textContent = message;
     target.classList.add('is-invalid');
   } else {
+    target.nextElementSibling.textContent = '';
     target.classList.remove('is-invalid');
   }
+}
+
+
+let validationRules = {
+  name : {exp: /^[a-zA-Z]{2,10}$/, msg: 'Name must be between 2 and 10 characters, bro'},
+  zip : {exp: /^[0-9]{5}(-[0-9]{4})?$/, msg: 'Enter a valid Zip Code'},
+  email : {exp: /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/, msg: 'Enter a valid Email Address'},
+  phone : {exp: /^\(?\d{3}\)?[-. ]?\d{3}[-. ]?\d{4}$/, msg: 'Enter a valid Phone Number'}
 }
