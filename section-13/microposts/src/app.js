dd9ad741c3848e78a3e13f79e294ac323c028a4a
Cody <codysechelski@gmail.com>
2018-04-18 00:19:58 -0500

    FINISHED!!!!!!!!!!!!

--- section-13/microposts/src/app.js
+++ section-13/microposts/src/app.js
@@ -7,6 +7,15 @@ document.addEventListener('DOMContentLoaded', getPosts);
 //listen for add post
 document.querySelector('.post-submit').addEventListener('click', submitPost);
 
+//listen for delete
+document.querySelector('#posts').addEventListener('click', deletePost);
+
+//listen for edit state
+document.querySelector('#posts').addEventListener('click', enableEdit);
+
+//listen for cancel
+document.querySelector('.card-form').addEventListener('click', cancelEdit);
+
 //Get Posts
 function getPosts() {
   http.get('http://localhost:3000/posts')
@@ -18,18 +27,108 @@ function getPosts() {
 function submitPost() {
   const title = document.querySelector('#title').value;
   const body = document.querySelector('#body').value;
+  const id = document.querySelector('#id').value;
 
-  const data = {
-    title,
-    body
+  if (title === '' || body === '') {
+    ui.showAlert('Post Title and Post Body are required','warning');
+  } else {
+
+    const data = {
+      title,
+      body
+    }
+
+    if (id === '') {
+      
+      //Create Post
+      http.post('http://localhost:3000/posts', data)
+        .then(data => {
+          ui.showAlert('Post Added', 'success');
+          ui.clearFields();
+          getPosts();
+        })
+        .catch(err => console.log(err));
+    } else {
+      http.put(`http://localhost:3000/posts/${id}`, data)
+        .then(data => {
+          ui.showAlert('Post Updated', 'success');
+          ui.changeFormState('add');
+          getPosts();
+        })
+        .catch(err => console.log(err));
+    }
+    
   }
+}
 
-  //Create Post
-  http.post('http://localhost:3000/posts', data)
-    .then(data => {
-      ui.showAlert('Post Added', 'success');
-      ui.clearFields();
-      getPosts();
-    })
-    .catch(err => console.log(err));
+function deletePost(e) {
+  if (e.target.classList.contains('delete') ||
+      e.target.parentElement.classList.contains('delete') ||
+      e.target.parentElement.parentElement.classList.contains('delete')) {
+    
+    //find the a tag
+    let target = e.target;
+    let link = e.target.nodeName.toLowerCase() === 'a' ? e.target : null;
+
+    while (link === null) {
+      target = target.parentElement;
+      if (target.nodeName.toLowerCase() === 'a') {
+        link = target;
+      }
+    }
+
+    //once the a tag is fond, get he data-id
+    const id = link.dataset.id;
+    
+    http.delete(`http://localhost:3000/posts/${id}`)
+      .then(data => {
+        ui.showAlert('Post Deleted', 'success');
+        getPosts();
+      })
+      .catch(err => console.log(err));
+    
+    e.preventDefault();
+  }
+}
+
+function enableEdit(e) {
+  if (e.target.classList.contains('edit') ||
+      e.target.parentElement.classList.contains('edit') ||
+      e.target.parentElement.parentElement.classList.contains('edit')) {
+    
+    //find the a tag
+    let target = e.target;
+    let link = e.target.nodeName.toLowerCase() === 'a' ? e.target : null;
+
+    while (link === null) {
+      target = target.parentElement;
+      if (target.nodeName.toLowerCase() === 'a') {
+        link = target;
+      }
+    }
+
+    //once the a tag is fond, get he data-id
+    const id = link.dataset.id;
+    const body = link.previousElementSibling.textContent;
+    const title = link.previousElementSibling.previousElementSibling.textContent;
+
+    const data = {
+      id,
+      title,
+      body
+    }
+
+    //fill the form
+    ui.fillForm(data);
+    
+
+    e.preventDefault();
+  }
+}
+
+function cancelEdit(e) {
+  if (e.target.classList.contains('post-cancel')) {
+    ui.changeFormState('add');
+    e.preventDefault();
+  }
 }
--- section-13/microposts/src/ui.js
+++ section-13/microposts/src/ui.js
@@ -5,6 +5,8 @@ class UI {
     this.bodyInput = document.querySelector('#body');
     this.idInput = document.querySelector('#id');
     this.postSubmit = document.querySelector('.post-submit');
+    this.cardForm = document.querySelector('.card-form');
+    this.formLastElement = document.querySelector('.form-end');
     this.formState = 'add';
   }
 
@@ -17,10 +19,10 @@ class UI {
           <div class="card-body">
             <h4 class="card-title">${post.title}</h4>
             <p class="card-text">${post.body}</p>
-            <a href="#" class="edit card-link" data-id="${post.id}">
+            <a href="#" class="edit card-link text-success" data-id="${post.id}">
               <i class="fal fa-edit"></i> Edit
             </a>
-            <a href="#" class="edit card-link text-danger" data-id="${post.id}">
+            <a href="#" class="delete card-link text-danger" data-id="${post.id}">
               <i class="fal fa-trash-alt"></i> Delete
             </a>
           </div>
@@ -36,27 +38,61 @@ class UI {
     this.bodyInput.value = '';
   }
 
+  fillForm(data) {
+    this.titleInput.value = data.title;
+    this.bodyInput.value = data.body;
+    this.idInput.value = data.id;
+    
+    this.changeFormState('edit');
+  }
+
+  changeFormState(type) {
+    if (type === 'edit') {
+      this.postSubmit.textContent = 'Update Post';
+      this.postSubmit.classList.remove('btn-primary');
+      this.postSubmit.classList.add('btn-warning');
+
+      //create a cancel button
+      const button = document.createElement('button');
+      button.className = 'post-cancel btn btn-outline-light btn-block';
+      button.appendChild(document.createTextNode('Cancel'));
+      this.cardForm.insertBefore(button, this.formLastElement);
+    }
+    else {
+      this.postSubmit.textContent = 'Post It';
+      this.postSubmit.classList.add('btn-primary');
+      this.postSubmit.classList.remove('btn-warning');
+
+      if (document.querySelector('.post-cancel')) {
+        document.querySelector('.post-cancel').remove();
+      }
+
+      this.idInput.value = '';
+      this.clearFields();
+    }
+  }
+
   showAlert(msg, className) {
     const alert = document.createElement('div');
     let icon;
     switch (className) {
       case 'success':
-        icon = 'fal fa-check';
+        icon = 'fal fa-fw fa-check';
         break;
       case 'info':
-        icon = 'fal fa-info-circle';
+        icon = 'fal fa-fw fa-info-circle';
         break;
       case 'warning':
-        icon = 'fal fa-exclamation-triangle';
+        icon = 'fal fa-fw fa-exclamation-triangle';
         break;
       case 'danger':
-        icon = 'fal fa-exclamation-circle';
+        icon = 'fal fa-fw fa-exclamation-circle';
         break;
       default:
-        icon = 'fal fa-bell';
+        icon = 'fal fa-fw fa-bell';
     }
     alert.className = `alert fade alert-${className}`;
-    alert.innerHTML = `<i class="${icon}"></i> ${msg}`;
+    alert.innerHTML = `<i class="${icon}"></i>&emsp;${msg}`;
     document.getElementById('alerts').appendChild(alert);
 
     setTimeout(function () {
