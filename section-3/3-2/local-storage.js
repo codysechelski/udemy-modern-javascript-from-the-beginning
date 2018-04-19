ddab15fed4b81c4a1b5ff53267968ccfd20dab49
sxuc3s <cody_sechelski@fanniemae.com>
2018-04-19 13:15:24 -0500

    creating a new branch just for the calorie app

--- section-3/3-2/local-storage.js
+++
@@ -1,4 +0,0 @@
-localStorage.setItem('firstName', 'Cody');
-localStorage.setItem('lastName', 'Sechelski');
-
-console.log(`${localStorage.getItem('firstName')} ${localStorage.getItem('lastName')}`);
--- section-9/9-1/js/app.js
+++
@@ -1,7 +0,0 @@
-try {
-  myfunction();
-} catch (e) {
-  console.log(e);
-  console.log(e.message);
-  console.log(e.name);
-}
